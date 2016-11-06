2bb051bb5615b350458f0a276cc24f4eb2bffcbc
mateuszdrwal <drwal.mateusz@gmail.com>
2016-11-06 10:26:09 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+channel = "lorgon"
+info = "Brian is just doing some random speedrun practice."
+ops = [u'lorgon', u'ajleece95', u'bergbergberg', u'conedodger240', u'greatscottlp', u'hiboymc', u'jahg1977', u'kinkajou1015', u'skinfan13', u'starrlett20', u'rallekralle11', u'frustbox', u'konotyran', u'mateuszdrwal', u'mbxdllfs']
+records = [69, 27196.0]
+ralle = "*hugs*"
