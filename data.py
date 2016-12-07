7790d6b51a4ffe98849f0020093ca36369854753
mateuszdrwal <drwal.mateusz@gmail.com>
2016-12-07 22:29:36 +0100

    done some more work on the responseHandler and fixed some bugs with spreadsheet updating

--- data.py
+++ data.py
@@ -1,8 +1,12 @@
 channel = "lorgon"
 info = "Brian is just doing some random speedrun practice."
 ops = [u'lorgon', u'ajleece95', u'bergbergberg', u'conedodger240', u'greatscottlp', u'hiboymc', u'jahg1977', u'kinkajou1015', u'skinfan13', u'starrlett20', u'rallekralle11', u'frustbox', u'konotyran', u'mateuszdrwal', u'mbxdllfs']
-records = [69, 27196.0]
 ralle = "*hugs*"
-joke = []
 quote = []
-easycommands = []
+joke = []
+commands = [
+['!addcommand', '!addcommand <command name> <command text> adds a simple command that displays text when its called. Ops only.', False, False],
+['!removecommand', '!removecommand <command> removes a command created by !addcommand. Ops only.', False, False],
+['!commands', 'displays all available commands', False, False],
+['!help', 'helpception', False, False],
+[u'!test', 'This command returns simple text.', True, True, u'test1']]
